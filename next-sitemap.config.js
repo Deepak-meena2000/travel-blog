e34712524175b7ab724b16b7d09f89cd31ce7260
Deepak-meena2000 <kumars.deepak2048@gmail.com>
2025-06-07 17:47:56 +0530

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://travel-blog-ruddy-one.vercel.app/",
+  siteUrl: "https://quickthrillguide.com/",
   generateRobotsTxt: true, // Also generate robots.txt
   changefreq: "daily",
   priority: 0.7,
