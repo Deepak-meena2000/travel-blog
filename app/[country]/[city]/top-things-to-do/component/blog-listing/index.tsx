9823430d556f9af701806622f4cff07b9f66e616
Deepak-meena2000 <kumars.deepak2048@gmail.com>
2025-06-07 13:08:21 +0530

    removed add banner

--- app/[country]/[city]/top-things-to-do/component/blog-listing/index.tsx
+++ app/[country]/[city]/top-things-to-do/component/blog-listing/index.tsx
@@ -41,7 +41,7 @@ const CityBlogListing = ({
 
   return (
     <>
-      <AddBanner />
+      {/* <AddBanner /> */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Main Content */}
         <div className="lg:col-span-2">
